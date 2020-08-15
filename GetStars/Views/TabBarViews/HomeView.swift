750b28a36fd118b6c6d329a9cbb19fb7833f8443
alemiranda1105 <amiranda110500@gmail.com>
2020-08-15 18:24:31 +0100

    CardView implementado

--- GetStars/Views/TabBarViews/HomeView.swift
+++ GetStars/Views/TabBarViews/HomeView.swift
@@ -9,30 +9,41 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var data: [Product] = [
+        Product(name: "Persona 1", description: "Futbolista", subtitle: "", image: "p1"),
+        Product(name: "Persona 2", description: "Motorista", subtitle: "", image: "p2")]
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
                     Button(action: { }){
                         Text("¡PÁSATE AL PRO!")
-                            .font(.system(size: 28, weight: .bold))
-                            .padding(32)
-                            .multilineTextAlignment(.center)
+                            .font(.system(size: 25, weight: .bold))
                         
                     }.frame(minWidth: 0, maxWidth: .infinity)
-                        .padding(14)
+                        .padding(48)
                         .background(RoundedRectangle(cornerRadius: 16)
                         .fill(Color.init("gris")))
                         .foregroundColor(Color.black)
-                    Spacer()
-                }.padding(.horizontal, 20)
+                    
+                    ForEach(0..<self.data.count, id: \.self) { item in
+                        ProductCard(item: self.$data[item])
+                    }
+                    
+                }.padding(.top, 20).padding(.horizontal, 32)
             }.navigationBarTitle("Inicio")
         }
     }
 }
 
+#if DEBUG
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        Group {
+            HomeView().previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+            HomeView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
     }
 }
+#endif
