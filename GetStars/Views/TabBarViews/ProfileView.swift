44b58b584d4c0a8738853abb0c757062205f8ac3
alemiranda1105 <amiranda110500@gmail.com>
2020-09-09 18:52:21 +0100

    Cambios en el perfil

--- GetStars/Views/TabBarViews/ProfileView.swift
+++ GetStars/Views/TabBarViews/ProfileView.swift
@@ -14,7 +14,10 @@ struct ProfileView: View {
     
     @EnvironmentObject var session: SessionStore
     
-    @State var visible: Bool = true
+    private var cats = ["Compras", "Autógrafos manuales"]
+    @State var catSeleccionada = 0
+    
+    @State var visible: Bool = false
     @State var downloading: Bool = true
     
     private func loadImage() {
@@ -34,72 +37,171 @@ struct ProfileView: View {
     }
     
     var body: some View {
-        NavigationView {
-            VStack {
-                if self.visible {
-                    Image(systemName: colorScheme == .dark ? "person.crop.circle": "person.crop.circle.fill")
-                    .resizable()
-                    .scaledToFill()
-                        .frame(width: 120, height: 120)
-                        .padding()
-                    
-                    Text(self.session.data?.getName() ?? "Dev")
-                        .font(.system(size: 20, weight: .thin))
-                    
-                    HStack(alignment: .center) {
-                        Button(action: {
-                            let impactMed = UIImpactFeedbackGenerator(style: .light)
-                            impactMed.impactOccurred()
-                            self.visible = false
-                        }) {
-                            Image(systemName: "arrow.up").resizable().frame(width: 22.0, height: 22.0)
-                        }.foregroundColor(colorScheme == .dark ? Color.white: Color.black).padding()
-                    }
+        GeometryReader { g in
+            NavigationView {
+                Group {
+                    if self.visible {
+                        Image(systemName: self.colorScheme == .dark ? "person.crop.circle": "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .padding()
                         
-                    Spacer()
-                    
-                } else {
-                    HStack {
-                        Button(action: {
-                            let impactMed = UIImpactFeedbackGenerator(style: .light)
-                            impactMed.impactOccurred()
-                            self.visible = true
-                        }) {
-                            Image(systemName: "arrow.down").resizable().frame(width: 22.0, height: 22.0)
-                        }.foregroundColor(colorScheme == .dark ? Color.white: Color.black).padding()
-                    }
-                    
-                    Spacer()
-                    
-                }
-                
-                if self.downloading {
-                    Text("Cargando...").font(.system(size: 32, weight: .heavy)).multilineTextAlignment(.center).padding()
-                } else {
-                    GridStack(minCellWidth: 125, spacing: 5, numItems: self.session.url.count){ i, width in
-                        NavigationLink(destination: AutographProfileView(url: self.session.url[i])) {
-                            WebImage(url: self.session.url[i].url)
-                            .resizable()
-                            .placeholder(Image(systemName: "photo"))
-                            .placeholder {
-                                Rectangle().foregroundColor(.gray)
+                        Text(self.session.data?.getName() ?? "Dev")
+                            .font(.system(size: 20, weight: .thin))
+                        
+                        HStack(alignment: .center) {
+                            Button(action: {
+                                let impactMed = UIImpactFeedbackGenerator(style: .light)
+                                impactMed.impactOccurred()
+                                self.visible = false
+                            }) {
+                                Image(systemName: "arrow.up").resizable().frame(width: 22.0, height: 22.0)
+                            }.foregroundColor(self.colorScheme == .dark ? Color.white: Color.black).padding()
+                        }
+                        
+                        Picker(selection: self.$catSeleccionada, label: Text("")) {
+                            ForEach(0 ..< self.cats.count) {
+                                Text(self.cats[$0])
                             }
-                            .indicator(.activity)
-                            .transition(.fade(duration: 0.5))
-                            .frame(width: width, height: width, alignment: .center)
-                            .border(Color.black, width: 1)
-                        }.buttonStyle(PlainButtonStyle()).padding(.vertical, 5)
+                        }.padding().pickerStyle(SegmentedPickerStyle())
+                            
+                        Spacer()
+                        
+                    } else {
+                        HStack {
+                            Button(action: {
+                                let impactMed = UIImpactFeedbackGenerator(style: .light)
+                                impactMed.impactOccurred()
+                                self.visible = true
+                            }) {
+                                Image(systemName: "arrow.down").resizable().frame(width: 22.0, height: 22.0)
+                            }.foregroundColor(self.colorScheme == .dark ? Color.white: Color.black).padding()
+                        }
+                        
+                        Picker(selection: self.$catSeleccionada, label: Text("")) {
+                            ForEach(0 ..< self.cats.count) {
+                                Text(self.cats[$0])
+                            }
+                        }.padding().pickerStyle(SegmentedPickerStyle())
+                        
+                        Spacer()
+                        
                     }
-                }
-                
-            }.navigationBarTitle(Text("Perfil"), displayMode: self.visible ? .automatic: .inline)
-            .navigationBarItems(trailing:
-                NavigationLink(destination: ConfigurationView().environmentObject(self.session)) {
-                    Image(systemName: "gear").resizable().frame(width: 28.0, height: 28.0)
-                }.foregroundColor(colorScheme == .dark ? Color.white: Color.black)
-            )
-        }.onAppear(perform: self.loadImage)
-        .navigationViewStyle(StackNavigationViewStyle())
+                    
+                    
+                    
+                    if self.downloading {
+                        Text("Cargando...").font(.system(size: 32, weight: .heavy)).multilineTextAlignment(.center).padding()
+                    } else {
+                        if self.catSeleccionada == 0 {
+                            ScrollView {
+                                NavigationLink(destination: Text("Foto")) {
+                                    VStack {
+                                        HStack {
+                                            VStack {
+                                                Text("Foto")
+                                                    .font(.system(size: 22, weight: .bold))
+                                                Spacer()
+                                                Image(systemName: "camera")
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60, alignment: .center)
+                                                    .scaledToFit()
+                                                Spacer()
+                                            }
+                                            Spacer()
+                                            
+                                            VStack {
+                                                Text("Foto dedicada: 18")
+                                                Text("Foto: 25")
+                                            }
+                                            
+                                        }.padding()
+                                        .background(RoundedRectangle(cornerRadius: 16).fill(Color("gris")))
+                                        .padding()
+                                    }
+                                }.foregroundColor(.black)
+                                
+                                NavigationLink(destination: Text("Autógrafo")) {
+                                    VStack {
+                                        HStack {
+                                            VStack {
+                                                Text("Autógrafo")
+                                                    .font(.system(size: 22, weight: .bold))
+                                                Spacer()
+                                                Image(systemName: "hand.draw")
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60, alignment: .center)
+                                                    .scaledToFit()
+                                                Spacer()
+                                            }
+                                            Spacer()
+                                            
+                                            VStack {
+                                                Text("Autógrafo dedicado: 30")
+                                                Text("Autógrafo: 25")
+                                            }.multilineTextAlignment(.center)
+                                            
+                                        }.padding()
+                                        .background(RoundedRectangle(cornerRadius: 16).fill(Color("gris")))
+                                        .padding()
+                                    }
+                                }.foregroundColor(.black)
+                                
+                                NavigationLink(destination: Text("Live")) {
+                                    VStack {
+                                        HStack {
+                                            VStack {
+                                                Text("Live")
+                                                    .font(.system(size: 22, weight: .bold))
+                                                Spacer()
+                                                Image(systemName: "hand.draw")
+                                                    .resizable()
+                                                    .frame(width: 60, height: 60, alignment: .center)
+                                                    .scaledToFit()
+                                                Spacer()
+                                            }
+                                            Spacer()
+                                            
+                                            VStack {
+                                                Text("Videos dedicados: 12")
+                                            }.multilineTextAlignment(.center)
+                                            
+                                        }.padding()
+                                        .background(RoundedRectangle(cornerRadius: 16).fill(Color("gris")))
+                                        .padding()
+                                    }
+                                }.foregroundColor(.black)
+                            }
+                        } else {
+                            GridStack(minCellWidth: 125, spacing: 5, numItems: self.session.url.count){ i, width in
+                                NavigationLink(destination: AutographProfileView(url: self.session.url[i])) {
+                                    WebImage(url: self.session.url[i].url)
+                                    .resizable()
+                                    .placeholder(Image(systemName: "photo"))
+                                    .placeholder {
+                                        Rectangle().foregroundColor(.gray)
+                                    }
+                                    .indicator(.activity)
+                                    .transition(.fade(duration: 0.5))
+                                    .frame(width: width, height: width, alignment: .center)
+                                    .border(Color.black, width: 1)
+                                }.buttonStyle(PlainButtonStyle()).padding(.vertical, 5)
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                }.navigationBarTitle(Text("Perfil"), displayMode: self.visible ? .automatic: .inline)
+                .navigationBarItems(trailing:
+                    NavigationLink(destination: ConfigurationView().environmentObject(self.session)) {
+                        Image(systemName: "gear").resizable().frame(width: 28.0, height: 28.0)
+                    }.foregroundColor(self.colorScheme == .dark ? Color.white: Color.black)
+                )
+            }.onAppear(perform: self.loadImage)
+            .navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
