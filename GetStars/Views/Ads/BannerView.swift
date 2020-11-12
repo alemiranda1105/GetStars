bbbad0fa66d85087fcbd13d61c46b00b2020e71b
alemiranda1105 <amiranda110500@gmail.com>
2020-11-12 20:22:59 +0000

    Build 2 de esta versión

--- GetStars/Views/Ads/BannerView.swift
+++ GetStars/Views/Ads/BannerView.swift
@@ -15,12 +15,6 @@ final private class BannerVC: UIViewRepresentable {
     func makeUIView(context: UIViewRepresentableContext<BannerVC>) -> GADBannerView {
         let banner = GADBannerView(adSize: kGADAdSizeBanner)
         banner.backgroundColor = UIColor.clear
-        #if DEBUG
-        banner.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        print("----------DEBUG----------")
-        #else
-        banner.adUnitID = "ca-app-pub-2307684125945843/7616092995"
-        #endif
         banner.adUnitID = "ca-app-pub-2307684125945843/7616092995"
         banner.rootViewController = UIApplication.shared.windows.first?.rootViewController
         banner.load(GADRequest())
@@ -28,9 +22,7 @@ final private class BannerVC: UIViewRepresentable {
         return banner
     }
     
-    func updateUIView(_ uiView: GADBannerView, context: UIViewRepresentableContext<BannerVC>) {
-        
-    }
+    func updateUIView(_ uiView: GADBannerView, context: UIViewRepresentableContext<BannerVC>) { }
 }
 
 struct Banner: View {
